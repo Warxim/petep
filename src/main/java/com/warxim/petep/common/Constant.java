590005ddfffa8ab397a422716c4385ee831dc35c
Michal Válka <michal.valka@warxim.com>
2023-11-10 14:25:35 +0100

    Increase version to 2.3.0beta

--- src/main/java/com/warxim/petep/common/Constant.java
+++ src/main/java/com/warxim/petep/common/Constant.java
@@ -30,7 +30,7 @@ import java.util.List;
 @PetepAPI
 public final class Constant {
     // PETEP version
-    public static final String VERSION = "2.2.0";
+    public static final String VERSION = "2.3.0beta";
 
     // PETEP web
     public static final String WEB = "https://petep.warxim.com/";
